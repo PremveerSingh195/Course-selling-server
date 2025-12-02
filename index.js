4f354e044c0de01d3becd4b0199a556a7c2767e1
Prem <premcallsmaster@gmail.com>
2025-12-02 18:30:37 +0530

    cdsahgf

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 
@@ -9,4 +10,12 @@ app.use("/user", userRouter);
 app.use("/course", courseRouter);
 app.use("/admin", adminRouter);
 
-app.listen(8000);
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://youtube:SdcZbvtt5fQEXsSy@harkiratclass.3incwu1.mongodb.net/"
+  );
+  app.listen(8000);
+  console.log("Server is running on PORT 3000");
+}
+
+main();
